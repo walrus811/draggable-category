4924cb8eb0c0274b2dd47a68120922e86386a93e
admin <walrus811@naver.com>
2019-12-27 17:52:31 +0900

    드래그 추가

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { CategoryNode, makeRootCategoryNode } from "./type/CategoryNode";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/style.css'
+import { node } from "prop-types";
 
 const root = makeRootCategoryNode(dummyData);
 root.printDepthFirstOrder();
@@ -11,17 +12,56 @@ class Node extends React.Component{
     constructor(props){
         super(props);
         this.handleClick=this.handleClick.bind(this);
+        this.handleDragStart=this.handleDragStart.bind(this);
+        this.handleDrop=this.handleDrop.bind(this);
+        this.handleOnDragOver=this.handleOnDragOver.bind(this);
+        this.handleDragEnd= this.handleDragEnd.bind(this);
     }
 
     handleClick(e){
         e.stopPropagation();
-        let {data1,data2}=this.props;
-        alert(data1);
+        let {nodeData}=this.props;
+        alert(nodeData.name);
     }
-    
+
+    handleDragStart(e){
+        e.stopPropagation();
+        let {nodeData}=this.props;
+        e.dataTransfer.dropEffect="move";
+        e.dataTransfer.setData("nodeId", nodeData.id);
+        console.log("handleDragStart");
+    }
+
+    handleDragEnd(e){
+        e.target.style.color = 'black';
+        e.preventDefault();
+    }
+
+    handleDrop(e) {
+        e.stopPropagation();
+        var data = e.dataTransfer.getData("nodeId"); 
+        console.log("handleDrop");
+        console.log(data);
+      }
+      
+    handleOnDragOver(e){
+        e.preventDefault();
+    }
+/* https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+Event	On Event Handler	Fires when…
+drag	ondrag	…a dragged item (element or text selection) is dragged.
+dragend	ondragend	…a drag operation ends (such as releasing a mouse button or hitting the Esc key; see Finishing a Drag.)
+dragenter	ondragenter	…a dragged item enters a valid drop target. (See Specifying Drop Targets.)
+dragexit	ondragexit	…an element is no longer the drag operation's immediate selection target.
+dragleave	ondragleave	…a dragged item leaves a valid drop target.
+dragover	ondragover	…a dragged item is being dragged over a valid drop target, every few hundred milliseconds.
+dragstart	ondragstart	…the user starts dragging an item. (See Starting a Drag Operation.)
+drop	ondrop	…an item is dropped on a valid drop target. (See Performing a Drop.)
+ */
+
     render() {
-        let {data1,data2}=this.props;
-        return <li draggable onClick={this.handleClick}>{data1}{data2}</li>
+        let {nodeData,childrenData}=this.props;
+        return <li draggable onClick={this.handleClick} onDragEnd={this.handleDragEnd} onDragOver={this.handleOnDragOver} onDragStart={this.handleDragStart} onDrop={this.handleDrop}>{nodeData.name}{childrenData}</li>
       }
 }
 
@@ -29,11 +69,11 @@ class RootNode extends React.Component {
     renderNode=(nodeData)=>{
         if (nodeData.children.length>0) {
             return ( 
-                <Node data1={nodeData.name} data2={<ul>{nodeData.children.map(item=> this.renderNode(item))}</ul>}/>
+                <Node nodeData={nodeData}  childrenData={<ul>{nodeData.children.map(item=> this.renderNode(item))}</ul>}/>
             );
         }
         else {
-            return <Node data1={nodeData.name}/>;
+            return <Node nodeData={nodeData}/>;
         }
     }
 
